607e1af86a20c4785ad3570d45f825b6d78a731e
wsoccorsi <wsoccorsi@gmail.com>
2019-08-30 09:30:12 -0400

    finally fixed thread error

--- De101.py
+++ De101.py
@@ -1,26 +1,5 @@
-import pygame
-from pygameWindow import PYGAME_WINDOW
-from constants import pygameWindowDepth, pygameWindowWidth
-from random import randint
+import sys
+sys.path.insert(0,'..')
+import Leap
 
-pw = PYGAME_WINDOW()
-x = pygameWindowWidth - pw.screen.get_width() // 2
-y = pygameWindowDepth - pw.screen.get_height() // 2
-
-def Perturb_Circle_Position():
-    fourSidedDieRoll = randint(1,4)
-    global x, y
-    if fourSidedDieRoll == 1:
-        x = x-1
-    elif fourSidedDieRoll == 2:
-        x = x+1
-    elif fourSidedDieRoll == 3:
-        y = y-1
-    else:
-        y = y+1
-
-while True:
-    pw.Prepare()
-    pw.Draw_Black_Circle(x, y)
-    pw.Reveal()
-    Perturb_Circle_Position()
+print "Worked"
