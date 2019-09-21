049cd86e47c3638123301ad00ac15266a0e65336
wsoccorsi <wsoccorsi@gmail.com>
2019-09-21 15:58:38 -0400

    few changes

--- Predict.py
+++ Predict.py
@@ -1,3 +1,19 @@
 import matplotlib.pyplot as plt
+from knn import KNN
+
 
 print(1)
+knn = KNN()
+knn.Load_Dataset('data/iris.csv')
+
+x = knn.data[:,0]
+y = knn.data[:,1]
+
+
+trainX = knn.data[::2,0:2]
+trainy = knn.target[::2]
+
+#visualization
+plt.figure()
+plt.scatter(x,y,c=knn.target)
+plt.show()
