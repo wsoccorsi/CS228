c56d5368bfd73da28a11c1dd3b7d0f4d65e1ed98
wsoccorsi <wsoccorsi@gmail.com>
2019-10-20 14:21:38 -0400

    logins

--- De17/Dict.py
+++ De17/Dict.py
@@ -6,8 +6,10 @@ print(database)
 userName = raw_input('Please enter your name: ')
 if userName in database:
     print('welcome back ' + userName + '.')
+    database[userName]['logins'] +=1
 else:
-    database[userName] = {}
+    database[userName] = { 'logins' : 1}
     print('welcome ' + userName + '.')
 
+print(database)
 pickle.dump(database,open('userData/database.p','wb'))
--- De17/userData/database.p
+++ De17/userData/database.p
@@ -1,2 +1,8 @@
 (dp0
-.
+S'Will'
+p1
+(dp2
+S'logins'
+p3
+I3
+ss.
